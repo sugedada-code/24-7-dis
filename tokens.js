6062fee60e251b4a2dcadbae52f6af9944f6a4c7
sugedada-code <sugedada@gmail.com>
2026-02-06 04:04:26 +0300

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -1,8 +1,8 @@
 // status can be "online", "idle", "dnd", or "invisible" or "offline"
 export default [
     {
-        channelId: "1344059457747026005",
-        serverId: "1344059457046319198",
+        channelId: "1254904198059917362",
+        serverId: "1254884649247834152",
         token: process.env.token1,
         selfDeaf: false,
         autoReconnect: {
